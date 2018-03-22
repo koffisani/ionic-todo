86aed4aef45d7557f7573d766460ba350409d8de
Koffi Sani <koffisani@gmail.com>
2018-03-22 19:00:26 +0000

    Ajout du bouton de création de nouvelles données

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = [];
+  constructor(public navCtrl: NavController, private alertController : AlertController) {
 
+  }
+
+  openTodoAlert() {
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Entrez votre Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData)=> {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todos.push(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
